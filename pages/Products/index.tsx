6e7161124f55163089761e98334890e10b542b06
Arnold <sulanaarnold@gmail.com>
2024-01-22 20:56:49 +0800

    added product infinite swiper, but to be continued

--- pages/Products/index.tsx
+++ pages/Products/index.tsx
@@ -94,6 +94,229 @@ const Products = () => {
               </ul>
             </div>
           </div>
+
+          <div className={styles.productsWrapper}>
+            <div className={styles.product}>
+              <div className={styles.image}>
+                <span>image here</span>
+              </div>
+              <div className={styles.info}>
+                <span className={styles.productName}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                  in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                  nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </span>
+                <div className={styles.priceAndSold}>
+                  <span className={styles.price}>&#8369;69</span>
+                  <span className={styles.sold}>10k+ sold</span>
+                </div>
+              </div>
+            </div>
+            <div className={styles.product}>
+              <div className={styles.image}>
+                <span>image here</span>
+              </div>
+              <div className={styles.info}>
+                <span className={styles.productName}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                  in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                  nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </span>
+                <div className={styles.priceAndSold}>
+                  <span className={styles.price}>&#8369;69</span>
+                  <span className={styles.sold}>10k+ sold</span>
+                </div>
+              </div>
+            </div>
+            <div className={styles.product}>
+              <div className={styles.image}>
+                <span>image here</span>
+              </div>
+              <div className={styles.info}>
+                <span className={styles.productName}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                  in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                  nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </span>
+                <div className={styles.priceAndSold}>
+                  <span className={styles.price}>&#8369;69</span>
+                  <span className={styles.sold}>10k+ sold</span>
+                </div>
+              </div>
+            </div>
+            <div className={styles.product}>
+              <div className={styles.image}>
+                <span>image here</span>
+              </div>
+              <div className={styles.info}>
+                <span className={styles.productName}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                  in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                  nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </span>
+                <div className={styles.priceAndSold}>
+                  <span className={styles.price}>&#8369;69</span>
+                  <span className={styles.sold}>10k+ sold</span>
+                </div>
+              </div>
+            </div>
+            <div className={styles.product}>
+              <div className={styles.image}>
+                <span>image here</span>
+              </div>
+              <div className={styles.info}>
+                <span className={styles.productName}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                  in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                  nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </span>
+                <div className={styles.priceAndSold}>
+                  <span className={styles.price}>&#8369;69</span>
+                  <span className={styles.sold}>10k+ sold</span>
+                </div>
+              </div>
+            </div>
+            <div className={styles.product}>
+              <div className={styles.image}>
+                <span>image here</span>
+              </div>
+              <div className={styles.info}>
+                <span className={styles.productName}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                  in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                  nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </span>
+                <div className={styles.priceAndSold}>
+                  <span className={styles.price}>&#8369;69</span>
+                  <span className={styles.sold}>10k+ sold</span>
+                </div>
+              </div>
+            </div>
+            <div className={styles.product}>
+              <div className={styles.image}>
+                <span>image here</span>
+              </div>
+              <div className={styles.info}>
+                <span className={styles.productName}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                  in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                  nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </span>
+                <div className={styles.priceAndSold}>
+                  <span className={styles.price}>&#8369;69</span>
+                  <span className={styles.sold}>10k+ sold</span>
+                </div>
+              </div>
+            </div>
+            <div className={styles.product}>
+              <div className={styles.image}>
+                <span>image here</span>
+              </div>
+              <div className={styles.info}>
+                <span className={styles.productName}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                  in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                  nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </span>
+                <div className={styles.priceAndSold}>
+                  <span className={styles.price}>&#8369;69</span>
+                  <span className={styles.sold}>10k+ sold</span>
+                </div>
+              </div>
+            </div>
+            <div className={styles.product}>
+              <div className={styles.image}>
+                <span>image here</span>
+              </div>
+              <div className={styles.info}>
+                <span className={styles.productName}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                  in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                  nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </span>
+                <div className={styles.priceAndSold}>
+                  <span className={styles.price}>&#8369;69</span>
+                  <span className={styles.sold}>10k+ sold</span>
+                </div>
+              </div>
+            </div>
+            <div className={styles.product}>
+              <div className={styles.image}>
+                <span>image here</span>
+              </div>
+              <div className={styles.info}>
+                <span className={styles.productName}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                  in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                  nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </span>
+                <div className={styles.priceAndSold}>
+                  <span className={styles.price}>&#8369;69</span>
+                  <span className={styles.sold}>10k+ sold</span>
+                </div>
+              </div>
+            </div>
+            <div className={styles.product}>
+              <div className={styles.image}>
+                <span>image here</span>
+              </div>
+              <div className={styles.info}>
+                <span className={styles.productName}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                  in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                  nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </span>
+                <div className={styles.priceAndSold}>
+                  <span className={styles.price}>&#8369;69</span>
+                  <span className={styles.sold}>10k+ sold</span>
+                </div>
+              </div>
+            </div>
+          </div>
           {/* <span className={styles.title}>On Sale</span> */}
         </div>
       </div>
